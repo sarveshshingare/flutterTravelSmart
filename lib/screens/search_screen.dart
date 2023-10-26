c5f1784cc05002f10a9ee1066dca066438f8c57b
sarveshshingare <sarveshss2513@gmail.com>
2023-10-26 13:31:10 +0530

    complete screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -75,6 +75,118 @@ class SearchScreen extends StatelessWidget {
           const Gap(40),
           const AppDoubleTextWidget(
               BigText: "Upcoming Metros", smallText: "View all"),
+          const Gap(15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 425,
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade200,
+                          blurRadius: 3,
+                          spreadRadius: 3)
+                    ],
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 190,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/sit.jpg"))),
+                    ),
+                    const Gap(20),
+                    Text(
+                      "20% discount on early booking. Don't miss the chance ",
+                      style: Styles.headlineStyle2,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            color: Color(0xff3ab8b8),
+                            borderRadius: BorderRadius.circular(18)),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount\nfor survey",
+                              style: Styles.headlineStyle2.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Gap(10),
+                            Text(
+                              "Take the survey about our services and get discounts ",
+                              style: Styles.headlineStyle2.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                          right: -45,
+                          top: -40,
+                          child: Container(
+                            padding: EdgeInsets.all(30),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                    width: 18, color: Color(0xff189999)),
+                                color: Colors.transparent),
+                          )),
+                    ],
+                  ),
+                  const Gap(15),
+                  Container(
+                    width: size.width * 0.44,
+                    height: 210,
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        color: Color(0xffec6545)),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Take love",
+                          style: Styles.headlineStyle2.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const Gap(5),
+                        RichText(
+                            text: const TextSpan(children: [
+                          TextSpan(text: '😍', style: TextStyle(fontSize: 30)),
+                          TextSpan(text: '🥰', style: TextStyle(fontSize: 42)),
+                          TextSpan(text: '😘', style: TextStyle(fontSize: 30))
+                        ]))
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
